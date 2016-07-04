802d357cddc57434b343ee6f4fc6ec18a1ed2780
praneet709 <praneetsingh709@gmail.com>
2016-07-04 12:06:42 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix stores a matrix X in memory
+## cacheSolve shows the inverse of a matrix if is in memory or computes the inverse and then shows the inverse
 
-## Write a short comment describing this function
-
+## makeCacheMatrix uses scoping rules and stores matrices in memory
 makeCacheMatrix <- function(x = matrix()) {
-
+inv <- NULL
+set <- function(y) {
+x <<- y
+inv <<- NULL
 }
-
-
-## Write a short comment describing this function
-
+get <- function() x
+setInverse <- function(inverse) inv <<- inverse
+getInverse <- function() inv
+list(set = set,
+get = get,
+setInverse = setInverse,
+getInverse = getInverse)
+}
+## cacheSolve uses corpcor, a library that avoids determinants and uses orthogonal descomposition
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+inv <- x$getInverse()
+if (!is.null(inv)) {
+message("getting cached data")
+return(inv)
+}
+mat <- x$get()
+inv <- solve(mat, ...)
+x$setInverse(inv)
+inv
 }
